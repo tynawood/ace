105d5bde3b4f44fc2c4145c18d5e582623e5552b
Haniel <wood.tyna@gmail.com>
2016-01-31 15:25:57 +0000

    fasfgafhasfas
    new update 1

--- geo/scripts/reload_geoplug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cordova plugin rm com.cowbell.cordova.geofence
-cordova plugin rm com.cowbell.cordova.geofence.tests
-
-cordova plugin add ~/dev/js/cordova/plugins/geofence
-cordova plugin add ~/dev/js/cordova/plugins/geofence/tests
-
-cordova prepare
